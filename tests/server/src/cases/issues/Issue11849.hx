014e61896703fbbb107b2b0fe00a5cedcb266737
Rudy Ges <k@klabz.org>
2024-12-02 15:00:31 +0100

    Add test

---
+++ tests/server/src/cases/issues/Issue11849.hx
@@ -0,0 +1,18 @@
+package cases.issues;
+
+class Issue11849 extends TestCase {
+	function test(_) {
+		var content = getTemplate("issues/Issue11849/Main.hx");
+		var transform = Markers.parse(content);
+		vfs.putContent("Main.hx", transform.source);
+
+		var args = ["-main", "Main"];
+		runHaxe(args);
+		assertSuccess();
+
+		runHaxeJsonCb(args, DisplayMethods.Hover, {file: new FsPath("Main.hx"), offset: transform.offset(1)}, res -> {
+			Assert.equals("bar", res.item.args.name);
+		});
+		assertSuccess();
+	}
+}
